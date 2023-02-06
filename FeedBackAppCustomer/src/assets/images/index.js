983f67289463037f835aac570c782154731bfb13
Mohd. Ehtesham <101743107+MohdEhtesham@users.noreply.github.com>
2023-02-06 17:08:36 +0530

    splash is done

---
+++ FeedBackAppCustomer/src/assets/images/index.js
@@ -0,0 +1,3 @@
+export const Logopath={
+    LOGO:require('./logo/logo.png')
+}
--- FeedBackAppCustomer/src/screens/splash/index.js
+++ FeedBackAppCustomer/src/screens/splash/index.js
@@ -1,13 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import style from './style'
+import { View, Text, Image } from 'react-native'
+import React, { useEffect } from 'react'
+import {  useNavigation } from '@react-navigation/native';
+import styles from './style'
+import { Logopath } from '../../assets/images';
 
-const Splash = () => {
+
+export default function Splash() {
+    const navigation =useNavigation
+    useEffect(()=>{
+        setTimeout(() => {
+            // navigation.replace()
+        }, 4000);
+    },[])
   return (
     <View>
-      <Text>Splash</Text>
+     <Image  source={Logopath.LOGO}/>
     </View>
   )
 }
-
-export default Splash

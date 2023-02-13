c8c59f37e5b1b3f061c615458696a2616deeaf44
Mohd. Ehtesham <101743107+MohdEhtesham@users.noreply.github.com>
2023-02-13 18:33:32 +0530

    modal is working now .

--- FeedBackAppCustomer/src/screens/addfeedback/style.js
+++ FeedBackAppCustomer/src/screens/addfeedback/style.js
@@ -1,5 +1,5 @@
 import { Dimensions, StyleSheet } from "react-native";
-import { ROBOTO_MEDIUM } from "../../assets/fonts";
+import { ROBOTO_LIGHT, ROBOTO_MEDIUM } from "../../assets/fonts";
 const {width, height}=  Dimensions.get('window');
 
 export default StyleSheet.create({
@@ -54,6 +54,43 @@ export default StyleSheet.create({
       },
       btn:{
         marginTop:'15%'
-      }
+      },
+      centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+      },
+      modalView: {
+        backgroundColor: 'white',
+        borderRadius: 20,
+        height:'55%',
+        width:'90%',
+        borderRadius: 20,
+        shadowOffset: {width: -2, height: 4},
+        shadowColor: '#171717',
+        shadowOpacity: 0.8,
+        shadowRadius: 8,
+        alignSelf:'center',
+        justifyContent:'center', 
+        marginTop:'40%'
+      },
+      modalText: {
+       fontFamily:ROBOTO_MEDIUM,
+       fontSize:25, 
+       margin:10, alignSelf:'center'
+      },
+      modalTex:{    
+        fontSize:ROBOTO_LIGHT, 
+        fontSize:15,
+         justifyContent:'center',  
+         alignItems:'center',   
+         alignSelf:'center'        
+        
+      }, 
+       btnhome:{
+        position:'absolute',
+        top:0,
+       }
 
 })
